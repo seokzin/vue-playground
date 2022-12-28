73142242374041aace81b946d8a362bcfd1c2082
seokzin <seokzin95@gmail.com>
2022-12-28 23:27:22 +0900

    feat: vue2 다운그레이드

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
