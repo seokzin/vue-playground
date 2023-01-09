a04bb54c0f314b10efd9102de33617bc14c909bf
seokzin <seokzin95@gmail.com>
2023-01-09 23:53:27 +0900

    feat: Router 추가
    - 네이밍 컨벤션
    - 간편한 import 방법?

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <h1>Hello World</h1>
   <CounterButton />
+
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    |
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  <RouterView />
 </template>
 
 <script setup>
 import CounterButton from '@/components/CounterButton.vue'
+import { RouterView, RouterLink } from 'vue-router'
 </script>
