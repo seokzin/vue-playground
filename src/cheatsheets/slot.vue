7b03c09b9839d29c25dfcf277e72e453189a6fba
seokzin <seokzin95@gmail.com>
2023-01-26 00:15:45 +0900

    feat: slot 샘플 컴포넌트

---
+++ src/cheatsheets/slot.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+import slotChild from './slotChild.vue'
+
+const msg = ref('Vue는 개발자에게 정말 유용하죠! 🎁')
+</script>
+
+<template>
+  <slotChild>부모로부터: {{ msg }}</slotChild>
+</template>
---
+++ src/cheatsheets/slotChild.vue
@@ -0,0 +1,4 @@
+<template>
+  <!-- slot은 react의 children 개념 + default 값을 간단하게 설정 가능 -->
+  <slot>대체: 부모로부터 컨텐츠를 못 받았어요! 😢</slot>
+</template>
