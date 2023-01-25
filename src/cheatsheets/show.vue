08c1b0c73934768ec1af1cb4c97555ab930b0925
seokzin <seokzin95@gmail.com>
2023-01-25 23:47:32 +0900

    feat: show 샘플 컴포넌트

---
+++ src/cheatsheets/show.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="toggleShow">Toggle</button>
+    <p v-if="show">This text is only shown when "show" is true</p>
+    <p v-show="show">
+      This text is always rendered, but hidden when "show" is false
+    </p>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+
+export default {
+  setup() {
+    const state = reactive({
+      show: true,
+    })
+    function toggleShow() {
+      state.show = !state.show
+    }
+    return {
+      ...toRefs(state),
+      toggleShow,
+    }
+  },
+}
+</script>
