0832bc121778d13ae71f5680e768ad89d37e0e60
seokzin <seokzin95@gmail.com>
2023-01-26 00:13:27 +0900

    feat: emit 샘플 컴포넌트

---
+++ src/cheatsheets/emit.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+import emitChild from './emitChild.vue'
+
+const childMsg = ref('자식 컴포넌트로부터 아직 메시지를 받지 못했어요!')
+</script>
+
+<template>
+  <emitChild @response="(msg) => (childMsg = msg)" />
+  <p>{{ childMsg }}</p>
+</template>
---
+++ src/cheatsheets/emitChild.vue
@@ -0,0 +1,9 @@
+<script setup>
+const emit = defineEmits(['response'])
+
+emit('response', '자식 컴포넌트로부터 🌷를 받았어요!')
+</script>
+
+<template>
+  <h2>자식 컴포넌트</h2>
+</template>
