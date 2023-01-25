61c676980a33654c77760710796aea24ec6b2713
seokzin <seokzin95@gmail.com>
2023-01-26 00:10:06 +0900

    feat: props 샘플 컴포넌트

---
+++ src/cheatsheets/props.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+import propsChild from './propsChild.vue'
+
+const greeting = ref('부모 컴포넌트로부터 💌을 전달받았어요!')
+</script>
+
+<template>
+  <propsChild :msg="greeting" />
+</template>
